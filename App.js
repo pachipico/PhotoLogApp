a620bd6991d83edda08463752c46c015ea1616b9
J.S <pachipico1@gmail.com>
2021-06-15 21:49:58 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+
+
+const App: () => Node = () => {
+
+
+  return (
+    <>
+  </>
+  );
+};
+
+
+
+export default App;
